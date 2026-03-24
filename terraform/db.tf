176e12f34837a8d0b4b2cfc9c03e614cc299a04c
Theodor Nygård <49783243+TheodorNygard@users.noreply.github.com>
2026-03-24 19:50:19 +0100

    Add Azure Cosmos DB resource configuration

--- terraform/db.tf
+++ terraform/db.tf
@@ -4,3 +4,15 @@ resource "azurerm_resource_group" "databases" {
 }
 
 # Cosmos Db
+resource "azurerm_cosmosdb_account" "db" {
+  name      = "nygdev-cosmos-db"
+  location  = var.location
+  resource_group_name = var.db_resource_group
+  kind  = "nosql"
+offer_type = "freetier"
+
+
+  consistency_policy {
+    consistency_level       = "Eventual"
+  }
+}
