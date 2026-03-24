e2ad801992ecab1dbcebe92da8ce0613ef9d9fa4
Theodor Nygård <49783243+TheodorNygard@users.noreply.github.com>
2026-03-24 19:55:25 +0100

    Refactor Cosmos DB resources and add SQL database/container
    Updated Cosmos DB configuration to use resource group name and location dynamically. Added SQL database and container resources with specified properties.

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,18 +1,41 @@
 resource "azurerm_resource_group" "databases" {
-  name      = var.db_resource_group
-  location  = var.location
+  name     = var.db_resource_group
+  location = var.location
 }
 
-# Cosmos Db
 resource "azurerm_cosmosdb_account" "db" {
-  name      = "nygdev-cosmos-db"
-  location  = var.location
-  resource_group_name = var.db_resource_group
-  kind  = "nosql"
-offer_type = "freetier"
-
+  name                = "nygdev-cosmos-db"
+  location            = azurerm_resource_group.databases.location
+  resource_group_name = azurerm_resource_group.databases.name
+  kind                = "GlobalDocumentDB"
+  offer_type          = "Standard"
+  enable_free_tier    = true
 
   consistency_policy {
-    consistency_level       = "Eventual"
+    consistency_level = "Eventual"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.databases.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "db"
+  resource_group_name = azurerm_resource_group.databases.name
+  account_name        = azurerm_cosmosdb_account.db.name
+  throughput          = 1000
+}
+
+resource "azurerm_cosmosdb_sql_container" "primary" {
+  name                = "primary"
+  resource_group_name = azurerm_resource_group.databases.name
+  account_name        = azurerm_cosmosdb_account.db.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_paths = ["/partition"]
+
+  indexing_policy {
+    indexing_mode = "none"
   }
 }
