d1ca3d7760c72d9a4939b50e2b0c78c98ff84318
Theodor Nygård <49783243+TheodorNygard@users.noreply.github.com>
2026-05-02 07:34:52 +0200

    Update default Ubuntu image offer version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,7 +49,7 @@ variable "vm_resource_group" {
 variable "ubuntu_offer" {
   description = "Ubuntu image offer name"
   type        = string
-  default     = "ubuntu-25_10"
+  default     = "ubuntu-26_04"
 }
 
 variable "db_resource_group" {
