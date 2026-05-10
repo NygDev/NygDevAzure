8e6204ec1f1a7eafd853a668c39a4b52abec6ad1
Theodor Nygaard <theodor@nygard.dev>
2026-05-10 12:06:23 +0200

    Update Program.cs

--- dotnetloggerapp/Program.cs
+++ dotnetloggerapp/Program.cs
@@ -2,6 +2,7 @@ using Azure.Identity;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
