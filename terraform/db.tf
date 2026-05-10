a6fc326d1341d600f9fe4917cdc96fea5bd596e2
Theodor Nygaard <theodor@nygard.dev>
2026-05-10 10:58:02 +0200

    Add function app SAMI to CosmosDb

--- terraform/db.tf
+++ terraform/db.tf
@@ -57,3 +57,11 @@ resource "azurerm_cosmosdb_sql_role_assignment" "my_user" {
   principal_id        = var.entra_owner_objectid
   scope               = azurerm_cosmosdb_account.db.id
 }
+
+resource "azurerm_cosmosdb_sql_role_assignment" "logger_function_app" {
+  resource_group_name = azurerm_resource_group.databases.name
+  account_name        = azurerm_cosmosdb_account.db.name
+  role_definition_id  = "${azurerm_cosmosdb_account.db.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
+  principal_id        = azurerm_function_app_flex_consumption.logger.identity[0].principal_id
+  scope               = azurerm_cosmosdb_account.db.id
+}
