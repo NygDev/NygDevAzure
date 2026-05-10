acf5ebd83872ad6d197bf491e39bb77206a1477b
Theodor Nygaard <theodor@nygard.dev>
2026-05-10 19:21:35 +0200

    Update cdn.tf

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -3,12 +3,4 @@ data "azurerm_storage_account" "nygdevcdn" {
   resource_group_name = var.cdn_resource_group
 }
 
-resource "azurerm_eventgrid_system_topic" "nygdevcdn_blob" {
-  name                   = "evgt-nygdevcdn-blob"
-  resource_group_name    = var.cdn_resource_group
-  location               = var.location
-  source_arm_resource_id = data.azurerm_storage_account.nygdevcdn.id
-  topic_type             = "Microsoft.Storage.StorageAccounts"
-  tags                   = local.common_tags
-}
 
