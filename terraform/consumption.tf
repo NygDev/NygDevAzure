674460e3e7b639ea0b5b3fe54dea5dfe7b120c49
Theodor Nygaard <theodor@nygard.dev>
2026-05-10 11:51:57 +0200

    Update consumption.tf

--- terraform/consumption.tf
+++ terraform/consumption.tf
@@ -107,7 +107,6 @@ resource "azurerm_function_app_flex_consumption" "this" {
 
   app_settings = {
     APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.consumption.connection_string
-    AzureWebJobsStorage__accountName      = azurerm_storage_account.consumption.name
   }
 
   tags = local.common_tags
@@ -141,7 +140,6 @@ resource "azurerm_function_app_flex_consumption" "logger" {
 
   app_settings = {
     APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.consumption.connection_string
-    AzureWebJobsStorage__accountName      = azurerm_storage_account.consumption.name
     CosmosDb__AccountEndpoint             = azurerm_cosmosdb_account.db.endpoint
   }
 
